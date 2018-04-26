fef64258ad332f5e5b6c474cb56909ec9fdca24e
CCDM07 <ccdm07@CCDM07.local>
2018-04-26 12:56:52 -0600

    Marvel parte 2

--- Marvel/js/index.js
+++ Marvel/js/index.js
@@ -4,6 +4,10 @@ var marvel = function(){
 		var url="http://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith="
 		var cantidadComics = 0;
 		var comics = "";
+		var cantidadSeries = 0;
+		var series = "";
+		var stories = "";
+		var cantidadStories = 0;
 		url = url + personaje;
 		$.ajax({
 			dataType:"json",
@@ -11,11 +15,37 @@ var marvel = function(){
 			success: function(response){
 				if(response.code == 200){
 					$("#foto").attr("src",response.data.results[0].thumbnail.path+"."+response.data.results[0].thumbnail.extension);
+					$("#nombre").html(response.data.results[0].name);
+					//$("#comics").html(response.data.results[0].)
+					cantidadComics=response.data.results[0].comics.returned;
+					for(var i=0;i<cantidadComics;i++){
+						comics+=response.data.results[0].comics.items[i].name+"<br>";
+					}
+					$("#comics").html("Comics: <br>"+comics);
+
+					cantidadSeries=response.data.results[0].series.returned;
+					for(var i=0;i<cantidadSeries;i++){
+						series+=response.data.results[0].series.items[i].name+"<br>";
+					}
+					$("#series").html("Series: <br>"+series);
+
+					cantidadStories=response.data.results[0].stories.returned;
+					for(var i=0;i<cantidadStories;i++){
+						stories+=response.data.results[0].stories.items[i].name+"<br>";
+					}
+					$("#stories").html("Stories: <br>"+stories);
 				}
 			}
 		});
-
 	}
-	$("#btnBuscar").on("click",Buscar);
+	var teclaPersonaje = function(tecla){
+		//Enter = 10 + 13
+		//Retorno de carro y avance de línea
+		if(tecla.which == 13){
+			Buscar();
+		}
+	}
+	$("#btnBuscar").on("click",Buscar)
+	$("#txtPersonaje").on("keypress",teclaPersonaje);
 }
 $(document).ready(marvel);
