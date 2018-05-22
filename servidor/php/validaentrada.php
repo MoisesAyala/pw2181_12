5dfbc4976c3c256f21cfd846ba9f7ed585198286
CCDM07 <ccdm07@CCDM07.local>
2018-05-22 13:05:14 -0600

    Validacion de usuarios

---
+++ servidor/php/validaentrada.php
@@ -0,0 +1,25 @@
+<?php 
+include 'conexiones.php'
+function valida(){
+	$usuario=$_POST["usuario"];
+	$clave	=md5($_POST["clave"]);
+	$con=conecta();
+	$consulta="select * from usuarios where usuario= '".$usuario."' and clave='".$clave. limit 1";
+	$resConsulta=mysqli_query($con,$consulta);
+	if(mysqli_num_rows($resConsulta))
+	 > 0){
+	 	$respuesta=true;
+}
+	$salidaJSON = array('respuesta' => $respuesta);
+	print json_encode($salidaJSON);
+	
+	$opc=$_POST["opc"];
+	switch ($opc) {
+		case 'validaentrada':
+			valida();
+			break;
+		
+		default:
+			break;
+	}
+ ?>
